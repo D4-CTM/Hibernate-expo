28be3622e3e32f1dbdc22715c4ad1ee8cc09d4df
D4-CTM <josuedelcid325@gmail.com>
2025-01-29 22:54:59 -0600

    inventory --It was a typo

--- src/main/java/com/exposition/Main.java
+++ src/main/java/com/exposition/Main.java
@@ -3,7 +3,6 @@ package com.exposition;
 import com.exposition.frontend.MainFrame;
 
 public class Main {
-
     public static void main(String[] args) {
         new MainFrame("Game stop").setVisible(true);
     }
